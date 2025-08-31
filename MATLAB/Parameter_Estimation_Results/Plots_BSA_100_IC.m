c4292e5c218ef5484bae1e7d81a6299e303cefb6
echacinr <163458500+echacinr@users.noreply.github.com>
2025-08-31 10:17:03 -0400

    Update Plots_BSA_100_IC.m
    Updated from Figure S4 to Figure S5

--- MATLAB/Parameter_Estimation_Results/Plots_BSA_100_IC.m
+++ MATLAB/Parameter_Estimation_Results/Plots_BSA_100_IC.m
@@ -22,8 +22,8 @@ exp_rel = xlsread(filename,sheet,xlRange);
 xlRange = 'AA3:AA13';
 exp_stdv = xlsread(filename,sheet,xlRange);
 
-figure(54) %Figure S4
-figname = 'figureS4';
+figure(55) %Figure S5
+figname = 'figureS5';
 subplot(2,2,3)
 
 hold on
@@ -114,4 +114,5 @@ end
 
 widthInches = 5;
 heightInches = 5;
+
 run('../ScriptForExportingImages.m')
